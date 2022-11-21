ebe5e5d2b772dbc8da64b2ddcc23fc776f5d7450
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-11-21 14:49:53 +0000

    First Test Export

---
+++ Assets/Scripts/DeathBarrier.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBarrier : MonoBehaviour
+{
+	
+	public string target = "n/a";
+	public int ownscene = 1;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider collison)
+    {
+        
+        if (collison.gameObject.tag == target)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(ownscene);
+        }
+
+    }
+	
+}
