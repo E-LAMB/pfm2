d181b2f564e5d4b2938c890a0f896467e9b48c1e
E-LAMB <113103562+E-LAMB@users.noreply.github.com>
2022-11-29 10:20:09 +0000

    Menu

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -6,11 +6,39 @@ using UnityEngine.SceneManagement;
 public class MenuButtons : MonoBehaviour
 {
 
-    public int loadtarget = 1;
-
-    public void loadGame()
+    public void load_scene_1()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(loadtarget);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void load_scene_2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+
+    public void load_scene_3()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(3);
+    }
+
+    public void load_scene_4()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(4);
+    }
+
+    public void load_scene_5()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(5);
+    }
+
+    public void load_scene_6()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(6);
+    }
+
+    public void load_scene_0()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 
     public void quitGame()
